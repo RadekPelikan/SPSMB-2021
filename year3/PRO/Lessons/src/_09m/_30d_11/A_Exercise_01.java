3ec4c19973c90a33ce498bb50bef3d8e3e0ff4fc
Radek Pelikán <radek.pelikan@student.spsmb.cz>
2021-09-30 12:13:22 +0200

    Updatovana dnesni lekce s dobrovolnym domacim ukolem

---
+++ year3/PRO/Lessons/src/_09m/_30d_11/A_Exercise_01.java
@@ -0,0 +1,9 @@
+package _09m._30d_11;
+
+public class A_Exercise_01 {
+
+    public static void main(String[] args) {
+        //Napište program, který metodou Eratosthenova síta vypíše všecha prvočísla od 1 do N, kdy N zadáme z klávesnice
+
+    }
+}
